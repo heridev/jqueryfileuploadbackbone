4f126c9631ae66d9b98f90a7b4c12cbaf49d3706
jose heriberto <heriberto.perez@crowdint.com>
2013-01-29 15:12:25 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jqueryfileuploadbackbone::Application.config.secret_token = 'bf2254d59901193fc48a2d7b99a0f273576f2e361127af4a45944688ab607264aab8f9c3a39a77493f28fdaa5b1cd57da497b3c505de78f631e7b4f7e7df133f'
